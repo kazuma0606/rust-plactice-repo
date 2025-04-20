1c5da8de1a714c10a76833ded107983c9fe0b5f3
kazuma0606 <yoshimura.hisa@gmail.com>
2025-04-20 23:10:54 +0900

    done done done

--- section4/section4-3/section4-3-1/src/lib.rs
+++ section4/section4-3/section4-3-1/src/lib.rs
@@ -1,12 +1,6 @@
-use pyo3::prelude::*;
+use wasm_bindgen::prelude::*;
 
-#[pyfunction]
-fn double(x: i32) -> i32 {
+#[wasm_bindgen]
+pub fn double(x: i32) -> i32 {
     x * 2
-}
-
-#[pymodule]
-fn myrustlib(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(double, m)?)?;
-    Ok(())
 }
---
+++ section4/section4-3/section4-3-1/src/test.js
@@ -0,0 +1,3 @@
+import { double } from '../pkg/section4_3_1.js';
+
+console.log(double(2)); // 4
